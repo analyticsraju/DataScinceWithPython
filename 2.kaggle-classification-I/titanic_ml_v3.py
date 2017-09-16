71fa53a09f60d766a4bf324357ed6fc468610bc5
Gangaraju Battem <31151331+analyticsraju@users.noreply.github.com>
2017-09-16 09:53:08 +0530

    Update titanic_ml_v3.py

--- 2.kaggle-classification-I/titanic_ml_v3.py
+++ 2.kaggle-classification-I/titanic_ml_v3.py
@@ -1,3 +1,5 @@
+#train the model with train data.
+#test data trained with model
 import pandas as pd
 print(pd.__version__)
 titanic_train = pd.read_csv("D:/Data/DataScience/Practice/titanic/train.csv")
@@ -12,7 +14,7 @@ titanic_train['Sex']
 titanic_train['Fare']
 titanic_train.Sex
 
-titanic_train['Survived']
+titanic_train['Survived']  
 titanic_train.groupby('Pclass').size()
 titanic_train.groupby(['Pclass', 'Survived']).size()
 titanic_train.groupby(['Pclass', 'Sex', 'Embarked', 'Fare', 'Survived']).size()
@@ -24,4 +26,7 @@ titanic_test.info()
 titanic_test['PassengerId']
 titanic_test.loc[titanic_test.Sex=='male','Survived'] = 0
 titanic_test.loc[titanic_test.Sex=='female','Survived'] = 1
+
+#file saving for kaggle submission for above 1.EDA,2.DP,3.FE,4.MB.
+#submission to kaggle.
 titanic_test.to_csv('D:/Data/DataScience/Practice/titanic/submission.csv', columns=['PassengerId','Survived'],index=False)
